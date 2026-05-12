df0bf63f95ccef9e6c5bc3dbc5c4e20e42e9f45e
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-05-12 15:59:50 +0200

    Update package directory and name in setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="ha-mqtt-sdk",
     version="0.1.0",
-    packages=find_packages("sdk"),
-    package_dir={"": "sdk"},
+    packages=find_packages("ha-mqtt-sdk"),
+    package_dir={"": "ha-mqtt-sdk"},
     install_requires=[
         "paho-mqtt>=1.6.1"
     ],
