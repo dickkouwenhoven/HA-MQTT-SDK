65e783274fd424bf0b0c790b67973883e11323b7
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-05-13 13:25:33 +0200

    Fix import statements in test_entity.py

--- tests/test_entity.py
+++ tests/test_entity.py
@@ -1,6 +1,6 @@
 import pytest
-import ha-mqtt-sdk.models.entity import Entity
-import ha-mqtt-sdk.config.domains import HADomain
+import ha_mqtt_sdk.models.entity import Entity
+import ha_mqtt_sdk.config.domains import HADomain
 
 def test_valid_entity():
 	entity = Entity(
