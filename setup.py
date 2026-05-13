9c6c40c84139d51421a0a50f40134b61636d15c6
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-05-13 13:33:37 +0200

    Rename package from ha-mqtt-sdk to ha_mqtt_sdk

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ha-mqtt-sdk",
+    name="ha_mqtt_sdk",
     version="0.1.0",
-    packages=find_packages("ha-mqtt-sdk"),
-    package_dir={"": "ha-mqtt-sdk"},
+    packages=find_packages("ha_mqtt_sdk"),
+    package_dir={"": "ha_mqtt_sdk"},
     install_requires=[
         "paho-mqtt>=1.6.1"
     ],
