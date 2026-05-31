90015c94a694d87ca6404966a29c0391da3ff94c
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-05-31 19:46:57 +0200

    Update async_entity_manager.py

--- ha_mqtt_sdk/core/async_entity_manager.py
+++ ha_mqtt_sdk/core/async_entity_manager.py
@@ -126,15 +126,20 @@ class AsyncEntityManager:
 		# Command handling
 		# ------------------------
 
-		# if entity.command_topic:
-		# 	await self._mqtt.subscribe(entity.command_topic)
 		topic = build_command_topic(
 			entity.domain,
 			entity.unique_id,
 			self._settings.discovery_prefix,
 		)
 
-		await self._mqtt.subscribe(topic)
+		if topic:
+			await self._mqtt.subscribe(topic)
+
+			_logger.debug(
+				"Subscribed to command topic: %s",
+				topic,
+			)
+			
 
 	async def update_state(self, entity: Entity, state: Any) -> None:
 		topic = build_state_topic(
