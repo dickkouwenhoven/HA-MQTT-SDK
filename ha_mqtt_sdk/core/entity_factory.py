179b6c14be0d796e85fb84b5f40be30de11d3629
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-05-31 11:13:50 +0200

    Change device_info type to Dict[str, Any]

--- ha_mqtt_sdk/core/entity_factory.py
+++ ha_mqtt_sdk/core/entity_factory.py
@@ -26,7 +26,7 @@ def create_entity(
 	domain: HADomain,
 	name: str,
 	unique_id: str,
-	device_info: Optional[Any] = None,
+	device_info: Optional[Dict[str,Any]] = None,
 	extra: Optional[Dict[str, Any]] = None,
 ) -> Entity:
 	"""
