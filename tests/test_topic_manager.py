b0e81e03a42f2f264c3aae31938abb2945c91f4b
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-01 20:06:47 +0200

    Enhance tests for topic manager functionality
    Added tests for command topic and invalid domain handling.

--- tests/test_topic_manager.py
+++ tests/test_topic_manager.py
@@ -1,3 +1,5 @@
+import pytest
+
 from ha_mqtt_sdk.builders.topic_manager import (
 	build_state_topic,
 	build_command_topic,
@@ -5,6 +7,8 @@ from ha_mqtt_sdk.builders.topic_manager import (
 	build_availability_topic,
 )
 from ha_mqtt_sdk.config.domains import HADomain
+
+from ha_mqtt_sdk.exceptions import BuilderError
 
 def test_state_topic():
 	topic = build_state_topic(
@@ -37,3 +41,20 @@ def test_discovery_topic():
                 "homeassistant",
         )
 	assert "config" in topic
+
+def test_sensor_has_no_command_topic():
+	topic = build_command_topic(
+		HADomain.SENSOR,
+		"id1",
+		"hoemassistant",
+	)
+
+	assert topic == ""
+
+def test_invalid_domain):
+	with pytest.raise(BuilderError):
+		build_state_topic(
+			"invalid",
+			"id1",
+			"homeassistant",
+		)
