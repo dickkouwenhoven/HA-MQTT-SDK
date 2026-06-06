a6e61a2d53781da00dad27a32e4dd2206cd524a4
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-06 10:31:24 +0200

    Add EntityManager and AsyncEntityManager imports

--- ha_mqtt_sdk/__init__.py
+++ ha_mqtt_sdk/__init__.py
@@ -16,9 +16,19 @@ Exports:
 
 Usage:
     from ha_mqtt_sdk import HASDK
+	
+	from ha_mqtt_sdk import EntityManager
+	from ha_mqtt_sdk import AsyncEntityManager
+	
 	from ha_mqtt_sdk import Entity
+	
 	from ha_mqtt_sdk import HADomain
 	from ha_mqtt_sdk import MQTTSettings
+	
+	from ha_mqtt_sdk import PahoMQTTClient
+	from ha_mqtt_sdk import AsyncMQTTClient
+	
+	from ha_mqtt_sdk import get_logger	
 """
 
 from .utils.logger import get_logger
@@ -28,15 +38,28 @@ LOGGER = get_logger(__name__)
 
 # Top-level imports for SDK users
 from .core.sdk import HASDK
+
+from .core.entity_manager import EntityManager
+from .core.async_entity_manager import AsyncEntityManager
+
 from .models.entity import Entity
+
 from .config.domains import HADomain
 from .config.mqtt import MQTTSettings
+
+from .mqtt.paho_client import PahoMQTTClient
+from .mqtt.async_client import AsyncMQTTClient
 
 __version__ = "1.0.0"
 
 __all__ = [
 	"HASDK",
+	"EntityManager",
+	"AsyncEntityManager",
 	"Entity",
 	"HADomain",
 	"MQTTSettings",
+	"PahoMQTTClient",
+	"AsyncMQTTClient",
+	"get_logger"
 ]
