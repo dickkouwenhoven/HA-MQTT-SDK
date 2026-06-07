974bdb5b4da09fa255924f4e6e5230746d8386eb
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-08 00:15:51 +0200

    Change 'Dict' to 'dict' in type annotations

--- ha_mqtt_sdk/mqtt/paho_client.py
+++ ha_mqtt_sdk/mqtt/paho_client.py
@@ -8,7 +8,7 @@ Used by:
 import json
 import threading
 import time
-from typing import Callable, Any, Dict, Optional
+from typing import Callable, Any, dict, Optional
 
 import paho.mqtt.client as mqtt
 
