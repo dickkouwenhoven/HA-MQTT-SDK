23b72e6ab65ab0f99ab28b834c062e555d3d0c73
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-07 14:34:51 +0200

    Modify DeviceInfo to support None for all attributes
    Updated DeviceInfo TypedDict to allow None values for all fields.

--- ha_mqtt_sdk/models/device_info.py
+++ ha_mqtt_sdk/models/device_info.py
@@ -1,13 +1,19 @@
 from typing import TypedDict
 
 class DeviceInfo(TypedDict, total=False):
-    identifiers: list[tuple[str, str]]
+    identifiers: list[tuple[str, str]] | None = None
     
-    manufacturer: str
-    model: str
-    name: str
+    manufacturer: str | None = None
+    model: str | None = None
+    name: str | None = None
     
-    sw_version: str
-    hw_version: str
+    sw_version: str | None = None
+    hw_version: str | None = None
     
-    suggested_area: str
+    suggested_area: str | None = None
+
+    connections: set[tuple[str, str]] | None = None
+    via_device: tuple[str, str] | None = None
+    configuration_url: str | None = None
+    serial_number: str | None = None
+    model_id: str | None = None
