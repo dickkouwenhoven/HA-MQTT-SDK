a2318321d923922e29cbe719d42b630df0e0c80a
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-07 23:43:34 +0200

    Reorder imports in main.py for clarity

--- examples/basic_usage/main.py
+++ examples/basic_usage/main.py
@@ -3,10 +3,10 @@ Basic example showing how to use the HASDK.
 """
 
 from ha_mqtt_sdk import (
-	HADomain,
-	HASDK,
-	MQTTSettings,
 	Entity,
+	HASDK,
+	HADomain,
+	MQTTSettings,
 	PahoMQTTClient,
 )
 
