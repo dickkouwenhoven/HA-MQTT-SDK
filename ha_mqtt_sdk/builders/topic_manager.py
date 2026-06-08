800764b956e88d830d93e069b3f00a571850a5e1
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-08 10:45:30 +0200

    Fix formatting and add space in topic_manager.py

--- ha_mqtt_sdk/builders/topic_manager.py
+++ ha_mqtt_sdk/builders/topic_manager.py
@@ -11,8 +11,8 @@ Used by:
 
 from ..config.device_fields import ALLOWED_FIELDS_PER_DOMAIN
 from ..config.domains import HADomain
-
 from ..exceptions import BuilderError
+
 
 def _validate_domain(domain: HADomain) -> None:
 	if not isinstance(domain, HADomain):
