7378952535e0ba23af6e4e771e070dead5a4ebbf
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-08 10:42:50 +0200

    Reorganize import statements in discovery_payload.py

--- ha_mqtt_sdk/builders/discovery_payload.py
+++ ha_mqtt_sdk/builders/discovery_payload.py
@@ -10,14 +10,15 @@ Used by:
 - sdk/core/entity_manager.py
 """
 from typing import Any
-from .topic_manager import (
-	build_command_topic,
-	build_state_topic,
-)
+
 from ..exceptions import EntityError
 from ..models.entity import Entity
 from ..utils.logger import get_logger
 from ..validators.payload_validator import validate_discovery_payload 
+from .topic_manager import (
+	build_command_topic,
+	build_state_topic,
+)
 
 _logger = get_logger(__name__)
 
