dee824fe494a72ee7eb30dcb7e0c827f7d824aec
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-09 12:41:13 +0200

    Change identifiers and connections from set to list

--- ha_mqtt_sdk/models/device_info.py
+++ ha_mqtt_sdk/models/device_info.py
@@ -2,7 +2,7 @@ from typing import TypedDict
 
 
 class DeviceInfo(TypedDict, total=False):
-    identifiers: set[tuple[str, str]] | None
+    identifiers: list[tuple[str, str]] | None
     
     manufacturer: str | None
     model: str | None
@@ -13,7 +13,7 @@ class DeviceInfo(TypedDict, total=False):
     
     suggested_area: str | None
 
-    connections: set[tuple[str, str]] | None
+    connections: list[tuple[str, str]] | None
     via_device: tuple[str, str] | None
     configuration_url: str | None
     serial_number: str | None
