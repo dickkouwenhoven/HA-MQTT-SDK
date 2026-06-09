dd4ef079f9abbc912c1628e9d64ad4bccd0f2907
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-09 12:12:53 +0200

    Refactor device info tests for clarity and coverage

--- tests/test_entity.py
+++ tests/test_entity.py
@@ -80,7 +80,7 @@ def test_valid_device_info():
 		},
 	).validate()
 	
-def test_device_info_missing_identifiers():
+def test_device_info_missing_identifiers_and_connections():
 	with pytest.raises(EntityError):
 		Entity(
 			domain=HADomain.SENSOR,
@@ -100,3 +100,123 @@ def test_device_info_identifier_not_tuple():
 			}
 		).validate()
 		
+def test_device_info_empty_identifiers():
+	with pytest.raises(EntityError):
+		Entity(
+			domain=HADomain.SENSOR,
+			name="Temp",
+			unique_id="temp_1",
+			device_info={
+				"identifiers": set(),
+			},
+		).validate()
+
+def test_device_info_identifier_wrong_tuple_length():
+	with pytest.raises(EntityError):
+		Entity(
+			domain=HADomain.SENSOR,
+			name="Temp",
+			unique_id="temp_1",
+			device_info={
+				"identifiers": {
+					("only_one_value",)
+				},
+			},
+		).validate()
+
+def test_device_info_identifier_empty_key():
+	with pytest.raises(EntityError):
+		Entity(
+			domain=HADomain.SENSOR,
+			name="Temp",
+			unique_id="temp_1",
+			device_info={
+				"identifiers": {
+					("", "device_1")
+				},
+			},
+		).validate()
+
+def test_device_info_identifier_empty_value():
+	with pytest.raises(EntityError):
+		Entity(
+			domain=HADomain.SENSOR,
+			name="Temp",
+			unique_id="temp_1",
+			device_info={
+				"identifiers": {
+					("ha_mqtt_sdk", "")
+				},
+			},
+		).validate()
+
+def test_valid_connections():
+	Entity(
+		domain=HADomain.SENSOR,
+		name="Temp",
+		unique_id="temp_1",
+		device_info={
+			"connections": {
+				("mac", "AA:BB:CC:DD:EE:FF")
+			},
+		},
+	).validate()
+
+def test_empty_connections():
+	with pytest.raises(EntityError):
+		Entity(
+			domain=HADomain.SENSOR,
+			name="Temp",
+			unique_id="temp_1",
+			device_info={
+				"connections": set(),
+			},
+		).validate()
+
+def test_valid_via_device():
+	Entity(
+		domain=HADomain.SENSOR,
+		name="Temp",
+		unique_id="temp_1",
+		device_info={
+			"identifiers": {
+				("ha_mqtt_sdk", "device_1")
+			},
+			"via_device": (
+				"ha_mqtt_sdk",
+				"gateway_1",
+			),
+		},
+	).validate()
+
+def test_invalid_via_device_length():
+	with pytest.raises(EntityError):
+		Entity(
+			domain=HADomain.SENSOR,
+			name="Temp",
+			unique_id="temp_1",
+			device_info={
+				"identifiers": {
+					("ha_mqtt_sdk", "device_1")
+				},
+				"via_device": (
+					"gateway_1",
+				)
+			},
+		).validate()
+
+def test_device_info_manufacturer_must_be_string():
+	with pytest.raises(EntityError):
+		Entity(
+			domain=HADomain.SENSOR,
+			name="Temp",
+			unique_id="temp_1",
+			device_info={
+				"identifiers": {
+					("ha_mqtt_sdk", "device_1")
+				},
+				"manufacturer": 123,
+			},
+		).validate()
+
+
