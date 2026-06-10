424c470d8320be81bc702d613653f35a8bf53d14
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-10 21:37:55 +0200

    Register command callback for entity in test

--- tests/test_async_sdk.py
+++ tests/test_async_sdk.py
@@ -273,6 +273,11 @@ async def test_async_set_command_callback():
     async def callback(topic, payload):
         pass
 
+    manager.register(
+        entity,
+        command_callback=callback
+    )
+
     manager.set_command_callback(
         entity,
         callback,
