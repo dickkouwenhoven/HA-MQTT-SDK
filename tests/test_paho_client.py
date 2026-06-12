0be0a434075bd93ec9700cfbf20f71ed85391d92
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-12 12:30:48 +0200

    Fix import statement for exceptions in test file

--- tests/test_paho_client.py
+++ tests/test_paho_client.py
@@ -3,7 +3,7 @@ from unittest.mock import MagicMock, patch
 
 import pytest
 
-from ..exceptions import MQTTError, ValidationError
+from .exceptions import MQTTError, ValidationError
 from ..mqtt.paho_client import PahoMQTTClient
 
 
