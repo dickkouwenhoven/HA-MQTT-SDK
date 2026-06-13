b4a796d4802d4e1e7ddb964c222de215d97d63d9
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-13 13:49:37 +0200

    Refactor patches for mqtt_client in async tests

--- tests/test_async_client.py
+++ tests/test_async_client.py
@@ -302,13 +302,15 @@ async def test_reconnect_loop_backoff(mqtt_client):
 
         mqtt_client._shutdown = True
 
-    with patch.object(
-        mqtt_client,
-        "_start_connection",
-        side_effect=start_connection,
-    ), patch(
-        "ha_mqtt_sdk.mqtt.async_client.asyncio.sleep",
-        AsyncMock(),
+    with (
+        patch.object(
+            mqtt_client,
+            "_start_connection",
+            side_effect=start_connection,
+        ), patch(
+            "ha_mqtt_sdk.mqtt.async_client.asyncio.sleep",
+            AsyncMock(),
+        ),
     ):
         await mqtt_client._reconnect_loop()
 
