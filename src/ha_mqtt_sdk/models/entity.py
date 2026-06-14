0fe7c905804a54e5a8035ce400b20a7ded27a4e5
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-14 12:25:11 +0200

    Change payload type in to_dict method to Any

--- src/ha_mqtt_sdk/models/entity.py
+++ src/ha_mqtt_sdk/models/entity.py
@@ -54,7 +54,7 @@ class Entity:
         self._validate_schema()
 
     def to_dict(self) -> dict[str, Any]:
-        payload: dict[str, str] = {
+        payload: dict[str, Any] = {
             "name": self.name,
             "unique_id": self.unique_id,
         }
