b6e4c67413c33bbf506333bc94a90390007be1d2
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-14 11:46:35 +0200

    Remove BaseAsyncMQTTClient from exports

--- src/ha_mqtt_sdk/mqtt/__init__.py
+++ src/ha_mqtt_sdk/mqtt/__init__.py
@@ -8,14 +8,12 @@ Provides:
 """
 
 from .async_client import AsyncMQTTClient
-from .base_async_mqtt_client import BaseAsyncMQTTClient
 from .base import BaseMQTTClient
 from .paho_client import PahoMQTTClient
 
 
 __all__ = [
     "BaseMQTTClient",
-    "BaseAsyncMQTTClient",
     "PahoMQTTClient",
     "AsyncMQTTClient",
 ]
