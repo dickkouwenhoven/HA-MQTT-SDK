0fabada2535f549993b663bec901a139817bca66
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-15 08:22:09 +0200

    Import build_registration in test_async_entity_manager

--- tests/test_async_entity_manager.py
+++ tests/test_async_entity_manager.py
@@ -8,6 +8,7 @@ from ha_mqtt_sdk.builders.topic_manager import (
 from ha_mqtt_sdk.config.domains import HADomain
 from ha_mqtt_sdk.config.mqtt import MQTTSettings
 from ha_mqtt_sdk.core.async_entity_manager import AsyncEntityManager
+from ha_mqtt_sdk.core.entity_factory import build_registration
 from ha_mqtt_sdk.exceptions import EntityError
 
 
