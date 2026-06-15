79bf8c5a718a75b0691a320b7063fdc96603a5e9
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-15 17:58:30 +0200

    Remove unnecessary blank line in test file

--- tests/mqtt/test_paho_client.py
+++ tests/mqtt/test_paho_client.py
@@ -6,7 +6,6 @@ from ha_mqtt_sdk.mqtt.config.mqtt import MQTTSettings
 from ha_mqtt_sdk.mqtt.exceptions import MQTTError, ValidationError
 from ha_mqtt_sdk.mqtt.paho_client import PahoMQTTClient
 
-
 # -----------------------------
 # Helpers
 # -----------------------------
