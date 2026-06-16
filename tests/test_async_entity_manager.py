ce9be97fa866e56beb660e394ebf94cbc0b85418
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-16 11:47:47 +0200

    Fix assertion call for set_message_callback

--- tests/test_async_entity_manager.py
+++ tests/test_async_entity_manager.py
@@ -556,7 +556,7 @@ def test_init_registers_message_callback(
         mqtt_settings=mqtt_settings,
     )
 
-    assert mqtt_client_async.set_message_callback.assert_called_once()
+    mqtt_client_async.set_message_callback.assert_called_once()
 
 
 @pytest.mark.asyncio
