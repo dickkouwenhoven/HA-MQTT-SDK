d3712ca6448ad9817ca4aa3f03b178b95f10697e
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-18 09:40:17 +0200

    Refactor test to use create_entity function

--- tests/models/test_entity.py
+++ tests/models/test_entity.py
@@ -1,16 +1,15 @@
-import pytest
-
 from ha_mqtt_sdk.config.domains import HADomain
-from ha_mqtt_sdk.exceptions import EntityError, SchemaError
+from ha_mqtt_sdk.core.entity_factory import create_entity
+from ha_mqtt_sdk.models.device_info import DeviceInfo
 from ha_mqtt_sdk.models.entity import Entity
 
 
 def test_to_dict_with_device_info():
     device_info: DeviceInfo = {}
 
-    device_info["manufacturer"] = "Ikea"
-    
-    entity = Entity(
+    device_info["manufacturer"] = "Ikea"    
+
+    entity: Entity = create_entity(
         domain=HADomain.SWITCH,
         name="Relay",
         unique_id="relay_1",
