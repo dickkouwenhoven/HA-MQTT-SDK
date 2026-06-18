59f68b06ec4dfdd2b1221a42ad2bb42a8e092bac
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-18 10:06:50 +0200

    Refactor SDK tests to use SDK instance methods

--- tests/core/test_sdk.py
+++ tests/core/test_sdk.py
@@ -1,10 +1,12 @@
 import pytest
 
 from ha_mqtt_sdk.config.domains import HADomain
+from ha_mqtt_sdk.config.mqtt import MQTTSettings
 from ha_mqtt_sdk.core.entity_factory import create_entity
 from ha_mqtt_sdk.core.sdk import HASDK
 from ha_mqtt_sdk.exceptions import SDKError
 from ha_mqtt_sdk.models.entity import Entity
+from ha_mqtt_sdk.mqtt.paho_client import PahoMQTTClient
 
 # -------------------------
 # Init tests
@@ -22,19 +24,40 @@ def test_register_with_invalid_entity():
 
 
 def test_update_state_with_invalid_entity():
+    mqtt_config = MQTTSettings(
+        host="localhost",
+        port=1883,
+    )
+
+    client = PahoMQTTClient(config=mqtt_config)
+
+    sdk = HASDK(
+        mqtt_client=client,
+    )
+
     with pytest.raises(SDKError):
-        HASDK.update_state("Invalid Entity", "ON")
+        sdk.update_state("Invalid Entity", "ON")
 
 
 def test_on_command_with_invalid_entity():
+    mqtt_config = MQTTSettings(
+        host="localhost",
+        port=1883,
+    )
+
+    client = PahoMQTTClient(config=mqtt_config)
+
+    sdk = HASDK(
+        mqtt_client=client,
+    )
+
     entity: Entity = create_entity(
         domain=HADomain.SWITCH,
         name="Relay",
         unique_id="relay_1",
     )
 
-    callback = "command_callback"
-
     entity.domain = "Invalid Domain"
+
     with pytest.raises(SDKError):
-        HASDK.on_command(entity, callback)
+        sdk.on_command(entity, "command_callback")
