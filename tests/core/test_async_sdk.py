bfb8b8d27a34acb536e162052fa9c019a935a7a3
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-21 11:47:57 +0200

    Clean up imports in test_async_sdk.py
    Removed unused imports from test_async_sdk.py

--- tests/core/test_async_sdk.py
+++ tests/core/test_async_sdk.py
@@ -1,11 +1,8 @@
 import pytest
 
-from ha_mqtt_sdk.config.domains import HADomain
 from ha_mqtt_sdk.config.mqtt import MQTTSettings
 from ha_mqtt_sdk.core.async_sdk import AsyncHASDK
-from ha_mqtt_sdk.core.entity_factory import create_entity
 from ha_mqtt_sdk.exceptions import SDKError
-from ha_mqtt_sdk.models.entity import Entity
 from ha_mqtt_sdk.mqtt.async_client import AsyncMQTTClient
 
 # -------------------------
