cca128e84305fbb1fb4a6d11f79cc4c9fd4fdfb9
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-21 12:25:10 +0200

    Remove unused imports from test_async_sdk.py
    Comment out unused imports in test_async_sdk.py

--- tests/core/test_async_sdk.py
+++ tests/core/test_async_sdk.py
@@ -1,15 +1,14 @@
 from unittest.mock import AsyncMock, MagicMock, patch
 
-import pytest
-
-from ha_mqtt_sdk.config.domains import HADomain
-from ha_mqtt_sdk.config.mqtt import MQTTSettings
-from ha_mqtt_sdk.core.async_sdk import AsyncHASDK
-from ha_mqtt_sdk.entity_factory import create_entity
-from ha_mqtt_sdk.exceptions import SDKError
-from ha_mqtt_sdk.models.entity import Entity
-from ha_mqtt_sdk.mqtt.async_client import AsyncMQTTClient
-
+# import pytest
+#
+# from ha_mqtt_sdk.config.domains import HADomain
+# from ha_mqtt_sdk.config.mqtt import MQTTSettings
+# from ha_mqtt_sdk.core.async_sdk import AsyncHASDK
+# from ha_mqtt_sdk.entity_factory import create_entity
+# from ha_mqtt_sdk.exceptions import SDKError
+# from ha_mqtt_sdk.models.entity import Entity
+# from ha_mqtt_sdk.mqtt.async_client import AsyncMQTTClient
 # -------------------------
 # Init tests
 # -------------------------
