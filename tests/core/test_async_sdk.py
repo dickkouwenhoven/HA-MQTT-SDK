79ba801741e3d9fb17847e86e772788cac6aa9cb
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-21 12:32:11 +0200

    Uncomment entity factory and SDKError imports
    Uncomment imports for create_entity and SDKError.

--- tests/core/test_async_sdk.py
+++ tests/core/test_async_sdk.py
@@ -5,9 +5,9 @@ import pytest
 from ha_mqtt_sdk.config.domains import HADomain
 from ha_mqtt_sdk.config.mqtt import MQTTSettings
 from ha_mqtt_sdk.core.async_sdk import AsyncHASDK
+from ha_mqtt_sdk.entity_factory import create_entity
+from ha_mqtt_sdk.exceptions import SDKError
 
-# from ha_mqtt_sdk.entity_factory import create_entity
-# from ha_mqtt_sdk.exceptions import SDKError
 # from ha_mqtt_sdk.models.entity import Entity
 # from ha_mqtt_sdk.mqtt.async_client import AsyncMQTTClient
 # -------------------------
