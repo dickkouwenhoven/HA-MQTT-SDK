f2ccdc6a7959598c9d8e0ff990be9beb1a944cb3
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-21 13:07:06 +0200

    Add BaseAsyncMQTTClient to module exports

--- src/ha_mqtt_sdk/mqtt/__init__.py
+++ src/ha_mqtt_sdk/mqtt/__init__.py
@@ -3,16 +3,19 @@ MQTT layer.
 
 Provides:
 - Base client interface
+- Base async client interface
 - Sync (Paho) implementation
 - Async implementation
 """
 
 from .async_client import AsyncMQTTClient
 from .base import BaseMQTTClient
+from .base_async_mqtt_client import BaseAsyncMQTTClient
 from .paho_client import PahoMQTTClient
 
 __all__ = [
     "BaseMQTTClient",
+    "BaseAsyncMQTTClient",
     "PahoMQTTClient",
     "AsyncMQTTClient",
 ]
