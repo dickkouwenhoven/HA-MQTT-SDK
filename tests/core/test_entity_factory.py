65e451136103940491fc3045fb7d59c204bd3f44
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-24 12:27:40 +0200

    Fix import order for SchemaError in test

--- tests/core/test_entity_factory.py
+++ tests/core/test_entity_factory.py
@@ -122,8 +122,9 @@ def test_build_registration_calls_dependencies_with_command_topic():
 def test_build_registration_unknown_domain_raises():
     """Line 125: missing schema entry must raise SchemaError."""
 
-    from ha_mqtt_sdk.exceptions import SchemaError
     from unittest.mock import patch
+
+    from ha_mqtt_sdk.exceptions import SchemaError
 
     entity = create_entity(
         domain=HADomain.SENSOR,
