b3820c7e722fa712e37ca9ef6e2b6d60be96eb64
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-25 14:45:08 +0200

    Reorder import statements in my_async_hub_plugin.py

--- examples/async_plugin_usage/my_async_hub_plugin.py
+++ examples/async_plugin_usage/my_async_hub_plugin.py
@@ -15,12 +15,12 @@ Responsibilities:
 import asyncio
 import contextlib
 
-from .my_async_hub import MyAsyncHub, MyAsyncHubDevice
-
 from ha_mqtt_sdk.config.domains import HADomain
 from ha_mqtt_sdk.core.async_plugin_interface import AsyncIntegrationPlugin
 from ha_mqtt_sdk.core.async_sdk import AsyncHASDK
 from ha_mqtt_sdk.models.entity import Entity
+
+from .my_async_hub import MyAsyncHub, MyAsyncHubDevice
 
 
 # Map hub device types to Home Assistant domains
