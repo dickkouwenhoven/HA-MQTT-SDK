5d546c04223ee14ee72a397670764ea05c1e1db6
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-25 14:32:50 +0200

    Remove duplicate import of MyHub and MyHubDevice

--- examples/plugin_usage/my_hub_plugin.py
+++ examples/plugin_usage/my_hub_plugin.py
@@ -14,11 +14,11 @@ Responsibilities:
 
 import threading
 
+from .my_hub import MyHub, MyHubDevice
 from ha_mqtt_sdk.config.domains import HADomain
 from ha_mqtt_sdk.core.plugin_interface import IntegrationPlugin
 from ha_mqtt_sdk.core.sdk import HASDK
 from ha_mqtt_sdk.models.entity import Entity
-from .my_hub import MyHub, MyHubDevice
 
 
 # Map hub device types to Home Assistant domains
