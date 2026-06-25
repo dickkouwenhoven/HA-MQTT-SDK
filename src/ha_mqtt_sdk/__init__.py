f2dab7c12f129aeb7a2bf790715ebd25112bcf8a
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-06-25 12:12:57 +0200

    Refactor imports and update module documentation

--- src/ha_mqtt_sdk/__init__.py
+++ src/ha_mqtt_sdk/__init__.py
@@ -1,5 +1,5 @@
 """
-Home Assistant MQTT SDK - Core Package
+Home Assistant MQTT SDK
 
 This is the main package initializer for the SDK.
 It exposes the top-level API for external SDK users.
@@ -11,48 +11,43 @@ Responsibilities:
 
 Exports:
 - HASDK
+- AsyncHASDK
 - Entity
 - HADomain
+- MQTTSettings
 - DeviceInfo
 - create_device_info
 - create_entity
+- AsyncMQTTClient
+- PahoMQTTClient
 
 Usage:
-    from ha_mqtt_sdk import HASDK
-    from ha_mqtt_sdk import AsyncHASDK
-
-    from ha_mqtt_sdk import Entity
-    from ha_mqtt_sdk import create_entity
-
+    from ha_mqtt_sdk import HASDK, AsyncHASDK
+    from ha_mqtt_sdk import Entity, create_entity
     from ha_mqtt_sdk import HADomain
-
-    from ha_mqtt_sdk import PahoMQTTClient
-    from ha_mqtt_sdk import AsyncMQTTClient
-
-    from ha_mqtt_sdk import DeviceInfo
-    from ha_mqtt_sdk import create_device_info
-
-    from ha_mqtt_sdk import get_logger
+    from ha_mqtt_sdk import MQTTSettings
+    from ha_mqtt_sdk import PahoMQTTClient, AsyncMQTTClient
+    from ha_mqtt_sdk import DeviceInfo, create_device_info
+    from ha_mqtt_sdk import IntegrationPlugin, AsyncIntegrationPlugin
 """
 
 from importlib.metadata import version
 
 from .config.domains import HADomain
+from .config.mqtt import MQTTSettings
+from .core.async_plugin_interface import AsyncIntegrationPlugin
 from .core.async_sdk import AsyncHASDK
 from .core.device_factory import create_device_info
 from .core.entity_factory import create_entity
+from .core.plugin_interface import IntegrationPlugin
 from .core.sdk import HASDK
 from .models.device_info import DeviceInfo
 from .models.entity import Entity
 from .mqtt.async_client import AsyncMQTTClient
 from .mqtt.paho_client import PahoMQTTClient
-from .utils.logger import get_logger
 
 # Version
 __version__ = version("ha_mqtt_sdk")
-
-# Central SDK logger
-LOGGER = get_logger(__name__)
 
 __all__ = [
     # Primary entry point
@@ -65,7 +60,10 @@ __all__ = [
     "HADomain",
     "DeviceInfo",
     "create_device_info",
+    "MQTTSettings",
     # MQTT Clients - needed for dependeny injection
     "PahoMQTTClient",
     "AsyncMQTTClient",
+    "IntegrationPlugin",
+    "AsyncIntegrationPlugin",
 ]
