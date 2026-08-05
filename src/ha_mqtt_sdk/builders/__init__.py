937406b4d50f898eac07d2fae3da43757aca70c4
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2026-08-05 16:13:22 +0200

    Add build_availability_topic to exports

--- src/ha_mqtt_sdk/builders/__init__.py
+++ src/ha_mqtt_sdk/builders/__init__.py
@@ -8,12 +8,14 @@ Responsible for:
 
 from .discovery_payload import build_discovery_payload
 from .topic_manager import (
+    build_availability_topic,
     build_command_topic,
     build_discovery_topic,
     build_state_topic,
 )
 
 __all__ = [
+    "build_availability_topic",
     "build_discovery_topic",
     "build_state_topic",
     "build_command_topic",
